c48cefb02fb5cde4a38e69d60f2d6cc259523c11
Faraz <farazpourebrahimh@gmail.com>
2025-02-13 15:05:30 +0330

    [ADD] input props to text input component

--- night-whisper/src/components/TextInput/TextInput.tsx
+++ night-whisper/src/components/TextInput/TextInput.tsx
@@ -1,18 +1,18 @@
-import { ReactNode } from "react";
+import {ComponentProps, ReactNode} from "react";
 import styles from "./TextInput.module.css";
 
-type Props = {
+type Props = ComponentProps<"input"> & {
   placeholder?: string;
   suffixIcon?: ReactNode;
 };
 
 export default function TextInput({
-  placeholder,
   suffixIcon,
+    ...otherProps
 }: Props): ReactNode {
   return (
     <div className={styles["text-input"]}>
-      <input type="text" placeholder={placeholder} />
+      <input type="text" {...otherProps}/>
       <div className={styles.suffix}>{suffixIcon}</div>
     </div>
   );
