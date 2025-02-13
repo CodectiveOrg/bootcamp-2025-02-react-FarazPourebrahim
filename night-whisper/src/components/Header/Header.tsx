05b6d46d12671407a0eb1bd3cc7cf345474b6ace
Faraz <farazpourebrahimh@gmail.com>
2025-02-13 14:38:30 +0330

    [PRETTIER]

--- night-whisper/src/components/Header/Header.tsx
+++ night-whisper/src/components/Header/Header.tsx
@@ -1,12 +1,10 @@
-import {ReactNode} from "react";
+import { ReactNode } from "react";
 import styles from "./Header.module.css";
 
 export default function Header(): ReactNode {
-    return (
-        <header className={styles.header}>
-            <h1>
-                Night Whisper
-            </h1>
-        </header>
-    )
+  return (
+    <header className={styles.header}>
+      <h1>Night Whisper</h1>
+    </header>
+  );
 }
