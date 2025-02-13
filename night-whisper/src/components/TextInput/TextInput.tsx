c8a560d75e2cc38a673d5d8379b8a3e5f374b7ee
Faraz <farazpourebrahimh@gmail.com>
2025-02-13 14:37:44 +0330

    [ADD] text input

---
+++ night-whisper/src/components/TextInput/TextInput.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+import styles from "./TextInput.module.css";
+
+type Props = {
+  placeholder?: string;
+  suffixIcon?: ReactNode;
+};
+
+export default function TextInput({
+  placeholder,
+  suffixIcon,
+}: Props): ReactNode {
+  return (
+    <div className={styles["text-input"]}>
+      <input type="text" placeholder={placeholder} />
+      <div className={styles.suffix}>{suffixIcon}</div>
+    </div>
+  );
+}
