f166ef52db39766976cd7ccc6540faa43305c54a
Faraz <farazpourebrahimh@gmail.com>
2025-02-13 22:52:38 +0330

    [FIX] toolbar style issues

--- night-whisper/src/components/Toolbar/Toolbar.tsx
+++ night-whisper/src/components/Toolbar/Toolbar.tsx
@@ -3,22 +3,25 @@ import styles from "./Toolbar.module.css";
 import TextInput from "../TextInput/TextInput.tsx";
 import MingcuteSearchLine from "../../icons/MingcuteSearchLine.tsx";
 import Select from "../Select/Select.tsx";
-import {SelectOption} from "../../types/select-option.type.ts";
+import { SelectOption } from "../../types/select-option.type.ts";
+import Button from "../Button/Button.tsx";
+import MingcuteMoonLine from "../../icons/MingcuteMoonLine.tsx";
 
 export default function Toolbar(): ReactNode {
-    const options: SelectOption[] = [
-        {label: "All", value: "all"},
-        {label: "Good", value: "good"},
-        {label: "Bad", value: "bad"},
-    ]
+  const options: SelectOption[] = [
+    { label: "All", value: "all" },
+    { label: "Good", value: "good" },
+    { label: "Bad", value: "bad" },
+  ];
 
   return (
     <div className={styles.toolbar}>
       <TextInput
+        className={styles.input}
         placeholder="Search notes..."
         suffixIcon={<MingcuteSearchLine />}
       />
-        <Select options={options} />
+      <Select options={options} />
       <Button variant="solid" size="medium" shape="square">
         <MingcuteMoonLine />
       </Button>
